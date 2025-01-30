76993d20cc500b1935954a307f66a526bf38f963
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-30 18:49:12 +0200

    convert t opnpm

--- src/hooks/useFirebaseAuth.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { FirebaseAuthContext } from "../context/firebase-auth-context";
-
-export default function useFirebaseAuth() {
-  return useContext(FirebaseAuthContext);
-}
